1863ba694503f93b9f2ded203f1e538ad80cdee2
Muhammet Şen <enmuhammet23@gmail.com>
2021-12-22 08:10:17 +0000

    Colorizing Brat

--- frontend/src/Brat.js
+++ frontend/src/Brat.js
@@ -6,57 +6,130 @@ import SkeletonInput from "antd/lib/skeleton/Input";
 
 
 var colldata = {
-  'entity_types': [
-    // this is optional
-    {
-      'type': 'SPAN_DEFAULT',
-      'bgColor': '#7fa2ff',
-      'borderColor': 'darken'
-    },
-    {
-      'type': 'ARC_DEFAULT',
-      'color': 'black',
-      'arrowHead': 'triangle,5',
-      'labelArrow': 'triangle,3,5',
-    },
-    {
-      'type': 'token',
-      'labels': ['\u00A0\u00A0'], // non-breaking space for empty
-    },
-    {
-      'type': '-',
-      'labels': ['\u00A0\u00A0'], // non-breaking space for empty
-    }
-  ],
-  'event_attribute_types': [],
-  'entity_attribute_types': [
-    {
-      'type': 'Name',
-      'values': {
-        'Name': { 'glyph': '(N)' },
-      },
-    },
-  ],
-  'relation_types': [
-   
-            {
-                'type': 'subj',
-                'labels': [ 'subj' ],
-                'dashArray': '3,3',
-                'color': 'green',
-                'args': [
-                    {
-                        'role': 'arg1',
-                        'targets': [ 'token' ]
-                    },
-                    {
-                        'role': 'arg2',
-                        'targets': [ 'token' ]
-                    }
-                ]
-            }
-  ],
-  'event_types': [],
+    'entity_types': [
+        // this is optional
+        {
+            'type': 'SPAN_DEFAULT',
+            'bgColor': '#E3E3E3',
+            'borderColor': 'darken'
+        },
+        {
+            'type': 'ARC_DEFAULT',
+            'color': 'black',
+            'arrowHead': 'triangle,5',
+            'labelArrow': 'triangle,3,5',
+        },
+        {
+            'type': 'NOUN',
+            'labels': ['Noun'],
+            'bgColor': '#a4bced',
+            'borderColor': 'darken'
+        },
+        {
+            'type': 'PROPN',
+            'labels': ['Propn'],
+            'bgColor': '#a4bced',
+            'borderColor': 'darken'
+        },
+        {
+            'type': 'VERB',
+            'labels': ['Verb'],
+            'bgColor': '#ADF6A2',
+            'borderColor': 'darken'
+        },
+        {
+            'type': 'AUX',
+            'labels': ['Aux'],
+            'bgColor': '#ADF6A2',
+            'borderColor': 'darken'
+        },
+        {
+            'type': 'PRON',
+            'labels': ['Pron', 'Pronoun'],
+            'bgColor': '#FFFDA8',
+            'borderColor': 'darken'
+        },
+        {
+            'type': 'ADJ',
+            'labels': ['Adj', 'Adjective'],
+            'bgColor': '#FFFDA8',
+            'borderColor': 'darken'
+        },
+        {
+            'type': 'ADV',
+            'labels': ['Adv', 'Adverb'],
+            'bgColor': '#FFFDA8',
+            'borderColor': 'darken'
+        },
+        {
+            'type': 'DET',
+            'labels': ['Det'],
+            'bgColor': '#CCADF6',
+            'borderColor': 'darken'
+        },
+        {
+            'type': 'NUM',
+            'labels': ['Num'],
+            'bgColor': '#CCADF6',
+            'borderColor': 'darken'
+        },
+        {
+            'type': 'ADP',
+            'labels': ['Adp'],
+            'bgColor': '#FFE8BE',
+            'borderColor': 'darken'
+        },
+        {
+            'type': 'PUNCT',
+            'labels': ['Punct'],
+            'bgColor': '#E3E3E3',
+            'borderColor': 'darken'
+        },
+        {
+            'type': 'CCONJ',
+            'labels': ['Cconj'],
+            'bgColor': '#FFFFFF',
+            'borderColor': 'darken'
+        },
+
+        {
+            'type': 'token',
+            'labels': ['\u00A0\u00A0'], // non-breaking space for empty
+        },
+        {
+            'type': '-',
+            'labels': ['\u00A0\u00A0'], // non-breaking space for empty
+        }
+    ],
+    'event_attribute_types': [],
+    'entity_attribute_types': [
+        {
+            'type': 'Name',
+            'values': {
+                'Name': { 'glyph': '(N)' },
+            },
+        },
+    ],
+    'relation_types': [
+
+        {
+            'type': 'subj',
+            'labels': ['subj'],
+            'dashArray': '3,3',
+            'color': 'green',
+            'args': [
+                {
+                    'role': 'arg1',
+                    'targets': ['token']
+                },
+                {
+                    'role': 'arg2',
+                    'targets': ['token']
+                }
+            ]
+        }
+    ],
+    'event_types': [],
 };
 
 class Brat extends React.Component {
