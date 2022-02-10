88b6e0c44104ed958bfe2d9e37b2daec7417742b
Muhammet Şen <enmuhammet23@gmail.com>
2022-02-10 15:04:42 +0300

    Changed json response structure - Include `message` field

--- backend/src/backend/backend_proxy/api/endpoints.py
+++ backend/src/backend/backend_proxy/api/endpoints.py
@@ -9,8 +9,8 @@ from backend.backend_proxy.tool.schema import ToolSchema
 from backend.backend_proxy.tool.service import ToolService
 from backend.backend_proxy.user.service import UserService
 from backend.backend_proxy.api.exception import REST_Exception
-from backend.backend_proxy.db.mongoDB import MongoDB
 from datetime import timedelta
+from backend.backend_proxy.db.mongoDB import MongoDB
 from backend.backend_proxy.misc.util import *
 import traceback
 import sys
@@ -31,7 +31,7 @@ def debugPrint(*args, **kwargs):
 
 @app.route("/api/alive", methods=["GET"])
 def alive():
-    return get_response_json("Hi", 200)
+    return create_response(message="Hi", status=200)
 
 
 @app.route("/api/tools", methods=["GET"])
@@ -39,12 +39,10 @@ def list_all_tools():
     try:
         data = ToolService().list_all_tools()
         status = 200
+        return create_response(data=data, status=status)
     except REST_Exception as e:
-        traceback.print_exc()
-        data = dict({"title": "Server Error",
-                     "subTitle": "Logs: {}".format(str(e))})
-        status = e.status
-    return get_response_json(data, status)
+        # traceback.print_exc()
+        return create_response(message=e.message, status=e.status)
 
 
 @app.route("/api/tools/name", methods=["GET"])
@@ -52,12 +50,10 @@ def get_tool_names():
     try:
         data = ToolService().list_all_tools()
         status = 200
+        return create_response(data=data, status=status)
     except REST_Exception as e:
-        traceback.print_exc()
-        data = dict({"title": "Server Error",
-                     "subTitle": "Logs: {}".format(str(e))})
-        status = e.status
-    return get_response_json(data, status)
+        # traceback.print_exc()
+        return create_response(message=e.message, status=e.status)
 
 
 @app.route("/api/tool", methods=["POST"])
@@ -69,70 +65,51 @@ def add_tool():
         token = request.headers.get("Token")
         req_dict = json.loads(request.data)
         req_dict = ToolService().add_tool(req_dict=req_dict, token=token)
-        data = dict({"title": "Tool is added to the proxy",
-                     "subTitle": "Tool Info: {}".format(json.dumps(req_dict))})
+        data = {"message": "Tool is added", "tool_info": req_dict}
         status = 200
+        return create_response(data=data, status=status)
     except REST_Exception as e:
-        traceback.print_exc()
-        data = dict({"title": "Server Error",
-                     "subTitle": "Logs: {}".format(str(e))})
-        status = e.status
-    return get_response_json(data, status)
+        # traceback.print_exc()
+        return create_response(message=e.message, status=e.status)
 
 
 @app.route("/api/tool/formats", methods=["GET"])
 def toolFormats():
     data = SupportedFormats.getSupportedTypes()
-    return get_response_json(data, 200)
+    return create_response(data=data, status=200)
 
 
-@app.route("/api/tool/<enum>", methods=["PUT"])
-def update_tool(enum):
-    try:
-        req_dict = json.loads(request.data)
-        req_dict = ToolService().update_tool(req_dict, enum, None)
-        data = dict({"title": "Tool is updated",
-                     "subTitle": "Tool Info: {}".format(json.dumps(req_dict))})
-        status = 200
-    except REST_Exception as e:
-        data = dict({"title": "Server Error",
-                     "subTitle": "Logs: {}".format(str(e))})
-        status = e.status
-    return get_response_json(data, status)
+# @app.route("/api/tool/<enum>", methods=["PUT"])
+# def update_tool(enum):
+#     try:
+#         req_dict = json.loads(request.data)
+#         req_dict = ToolService().update_tool(req_dict, enum, None)
+#         data = {"message": "Tool is updated", "tool_info": req_dict}
+#         status = 200
+#         return create_response(data=data, status=status)
+#     except REST_Exception as e:
+#         return create_response(message=e.message, status=e.status)
 
-
-@app.route("/api/tool/<enum>", methods=["DELETE"])
-def delete_tool(enum):
-    try:
-        access_tools = UserService().get_tools_user(session)
-        UserService().delete_tool(access_tools, enum)
-        tool_json = ToolService().delete_tool(enum, access_tools)
-        data = dict({"title": "Tool is deleted",
-                     "subTitle": "Tool Info: {}".format(json.dumps(tool_json))})
-        status = 200
-    except REST_Exception as e:
-        data = dict({"title": "Server Error",
-                     "subTitle": "Logs: {}".format(str(e))})
-        status = e.status
-    return get_response_json(data, status)
-
+# @app.route("/api/tool/<enum>", methods=["DELETE"])
+# def delete_tool(enum):
+#     pass
 
 @app.route("/api/tool/run/<enum>", methods=["POST"])
 def run_tool(enum):
     event = Event(enum)
+    res = None
     try:
         # input for the tool
         input_dict = json.loads(request.data)
         data = ToolService().run_tool(enum, input_dict)
         status = 200
+        res = create_response(data=data, status=status)
     except REST_Exception as e:
-        traceback.print_exc()
-        data = dict({"title": "Server Error",
-                     "subTitle": "Logs: {}".format(str(e))})
-        status = e.status
+        # traceback.print_exc()
+        res = create_response(message=e.message, status=e.status)
     event.finish_event(status)
     debugPrint(event)
-    return get_response_json(data, status)
+    return res
 
 
 @app.route("/api/tool/restart/<enum>", methods=["POST"])
@@ -143,14 +120,13 @@ def restart_tool(enum):
         data = DockerService().restart_container(enum, input_dict)
         if (data):
             ToolService().toolObjects[enum].port = data
-            data = True
-        status = 200
+            data = {"message": f"Restarted {enum}"}
+            status = 200
+            return create_response(data=data, status=status)
+        return create_response(message=f"Could not restart {enum}", status=400)
     except REST_Exception as e:
-        traceback.print_exc()
-        data = dict({"title": "Server Error",
-                     "subTitle": "Logs: {}".format(str(e))})
-        status = e.status
-    return get_response_json(data, status)
+        # traceback.print_exc()
+        return create_response(message=e.message, status=e.status)
 
 # =============================================================
 
@@ -161,11 +137,10 @@ def login_user():
         req_dict = json.loads(request.data)
         data = UserService().login_user(req_dict)
         status = 200
+        return create_response(data=data, status=status)
     except REST_Exception as e:
-        data = dict({"title": "Server Error",
-                     "subTitle": f"Logs: {str(e)}"})
-        status = e.status
-    return get_response_json(data, status)
+        # traceback.print_exc()
+        return create_response(message=e.message, status=e.status)
 
 
 @app.route("/api/users", methods=["GET"])
@@ -178,11 +153,10 @@ def get_users():
     try:
         data = UserService().get_users(token=token)
         status = 200
+        return create_response(data=data, status=status)
     except REST_Exception as e:
-        data = dict({"title": "Server Error",
-                     "subTitle": "Logs: {}".format(str(e))})
-        status = e.status
-    return get_response_json(data, status)
+        # traceback.print_exc()
+        return create_response(message=e.message, status=e.status)
 
 
 @app.route("/api/user/register", methods=["POST"])
@@ -191,22 +165,23 @@ def register_user():
         req_dict = json.loads(request.data)
         is_successful = UserService().create_user(req_dict)
         if is_successful:
-            data = dict({"title": "Register success", })
-            status = 200
+            data = {"message": "Registered Successfully"}
+            return create_response(data=data, status=200)
         else:
-            data = dict({"title": "Could not register", })
-            status = 400
-
+            data = {"message": "Could not register"}
+            return create_response(data=data, status=400)
     except REST_Exception as e:
-        data = dict({"title": "Server Error",
-                     "subTitle": "Logs: {}".format(str(e))})
-        status = e.status
-    return get_response_json(data, status)
+        # traceback.print_exc()
+        return create_response(message=e.message, status=e.status)
 
 
-def get_response_json(data, status):
-    response = (json.dumps(data), status)
-    return response
+def create_response(data: dict = {}, message: str = None, status: int = 400) -> tuple[dict, int]:
+    res = {}
+    if status != 200:
+        res['message'] = message
+    else:
+        res = data
+    return (json.dumps(res), status)
 
 
 if __name__ == '__main__':
