ebc8a4d06ba89de0e1c46463f3ccb491192421ab
Muhammet Şen <enmuhammet23@gmail.com>
2022-02-11 12:17:38 +0300

    Commented tests for previous architecture

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -3,7 +3,7 @@ from backend.backend_proxy.tool.service import ToolService
 
 import pytest 
 
-@pytest.fixture(scope="session")
-def toolService():
-    MongoDB.getInstance("mongodb://localhost:27017/")
-    return ToolService()
+# @pytest.fixture(scope="session")
+# def toolService():
+#     MongoDB.getInstance("mongodb://localhost:27017/")
+#     return ToolService()
--- backend/tests/dockerService/test_docker_singleton.py
+++ backend/tests/dockerService/test_docker_singleton.py
@@ -1,15 +1,15 @@
 from backend.backend_proxy.containerization.service import DockerService
 from random import randint
 
-def test_singleton():
-    """Tool Service must be singleton"""
+# def test_singleton():
+#     """Tool Service must be singleton"""
 
-    # We do not want to initialize the instance 
-    object1 = object.__new__(DockerService)
-    object2 = object.__new__(DockerService)
+#     # We do not want to initialize the instance 
+#     object1 = object.__new__(DockerService)
+#     object2 = object.__new__(DockerService)
     
-    randomValue = randint(0,1000)
-    object1.randomValue = randomValue
-    object2.randomValue = randomValue
-    assert object1.randomValue == object2.randomValue 
+#     randomValue = randint(0,1000)
+#     object1.randomValue = randomValue
+#     object2.randomValue = randomValue
+#     assert object1.randomValue == object2.randomValue 
     
