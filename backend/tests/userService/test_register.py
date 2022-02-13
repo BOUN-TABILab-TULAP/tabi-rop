4cb6c3f36f106ea04e96ff2dbfc00e64c4cff28a
Muhammet Şen <enmuhammet23@gmail.com>
2022-02-13 15:03:36 +0300

    Admin token bug in register tests

--- backend/tests/userService/test_register.py
+++ backend/tests/userService/test_register.py
@@ -7,14 +7,20 @@ from mock_user_controller import MockUserController
 
 userService = UserService(controller=MockUserController())
 
-
+def get_admin_token():
+    req = {
+        "username": "user3",
+        "password": "password3"
+    }
+    res = userService.login_user(req)
+    return res['token']
 def test_register_successful():
     req = {
         "username": f"user_{randint(0,10e5)}",
         "password": "password5",
         "email": "email5",
     }
-    res = userService.create_user(req_dict=req,token="token3")
+    res = userService.create_user(req_dict=req,token=get_admin_token())
     assert res == True
 
 
@@ -25,7 +31,7 @@ def test_register_user_exists():
         "email": "email5",
     }
     try:
-        res = userService.create_user(req_dict=req,token="token3")
+        res = userService.create_user(req_dict=req,token=get_admin_token())
     except REST_Exception as e:
         assert e.message == "Username is already taken"
         assert e.status == 400
@@ -38,7 +44,7 @@ def test_register_no_username():
         "email": "email5",
     }
     try:
-        res = userService.create_user(req_dict=req,token="token3")
+        res = userService.create_user(req_dict=req,token=get_admin_token())
     except REST_Exception as e:
         assert e.message == "You must provide an username"
         assert e.status == 400
@@ -50,7 +56,7 @@ def test_register_no_password():
         "email": "email5",
     }
     try:
-        res = userService.create_user(req_dict=req,token="token3")
+        res = userService.create_user(req_dict=req,token=get_admin_token())
     except REST_Exception as e:
         assert e.message == "You must provide a password"
         assert e.status == 400
@@ -62,7 +68,7 @@ def test_register_no_email():
         "password": "password6",
     }
     try:
-        res = userService.create_user(req_dict=req,token="token3")
+        res = userService.create_user(req_dict=req,token=get_admin_token())
     except REST_Exception as e:
         assert e.message == "You must provide an email"
         assert e.status == 400
