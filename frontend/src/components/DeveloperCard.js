1238c8373ed0efa93ae2174e20bc80fe5559dc9c
Muhammet Şen <enmuhammet23@gmail.com>
2022-08-02 20:18:58 +0300

    Add department links to about page

--- frontend/src/components/DeveloperCard.js
+++ frontend/src/components/DeveloperCard.js
@@ -1,29 +1,32 @@
 import * as React from 'react';
 
 import { makeStyles } from '@mui/styles';
-import { Card, CardContent, Typography,CardMedia, CardActions,Button } from '@mui/material/';
+import { Card, CardContent, Typography,CardMedia, CardActions,Button, Link } from '@mui/material/';
 import InfoIcon from '@mui/icons-material/Info';
 import { useTranslation } from 'react-i18next';
 const useStyles = makeStyles({
   card: {
     width: "22em",
     marginLeft: "0.7em",
-    marginBottom: "0.2em",
+    marginBottom: "0.7em",
     padding:"0.2em",
     alignItems:"center",
     justifyContent:"space-between",
     display:"flex",
     flexDirection:"column",
   
-
   },
   media:{
       width: '150px !important',
       height: '150px !important',
       borderRadius: '50%',
   }
-
 });
+
+const departmentURLS = {
+  "Department of Computer Engineering":"https://www.cmpe.boun.edu.tr/",
+  "Department of Linguistics":"https://linguistics.boun.edu.tr/"
+}
 
 export default function DeveloperCard({developer}) {
   const { t } = useTranslation()
@@ -44,8 +47,14 @@ export default function DeveloperCard({developer}) {
           {`${developer.firstName} ${developer.lastName}`}
         </Typography>
         <Typography variant="body2"  align="center" color="text.secondary">
-          {developer.info}
+          {`${developer.affiliation} of `}
+          <Link href={departmentURLS[developer.department]} target="_blank" underline="hover" color="inherit">
+            {developer.department}
+          </Link>
         </Typography>
+        {developer.info?.length > 0 && <Typography variant="body2"  align="center" color="text.secondary">
+          {developer.info}
+        </Typography>}
         
       </CardContent>
       <CardActions>
